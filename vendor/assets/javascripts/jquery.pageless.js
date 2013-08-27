09274d4ed3db3018f765a27fb72dbe4b080912c8
Hyo Seong Choi <rorlab@gmail.com>
2013-08-28 00:38:05 +0900

    added load1.gif

--- vendor/assets/javascripts/jquery.pageless.js
+++ vendor/assets/javascripts/jquery.pageless.js
@@ -101,7 +101,7 @@
   var loaderHtml = function () {
     return settings.loaderHtml ||
       '<div id="pageless-loader" style="display:none;text-align:center;width:100%;">' +
-      '<div class="msg" style="color:#e9e9e9;font-size:2em"></div>' +
+      '<div class="msg" style="color:#e9e9e9;font-size:1em"></div>' +
       '<img src="' + settings.loaderImage + '" alt="loading more results" style="margin:10px auto" />' +
       '</div>';
   };
